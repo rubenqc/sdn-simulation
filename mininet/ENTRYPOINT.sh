25b9fa8b6a8e075624673d614aefa2e93b00652f
Ruben Quispe <rwquispec@gmail.com>
2022-06-18 17:11:57 -0500

    Init project sdn-simulation

---
+++ mininet/ENTRYPOINT.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+service openvswitch-switch start
+ovs-vsctl set-manager ptcp:6640
+
+bash
---
+++ script.sh
@@ -0,0 +1,1 @@
+feature:install odl-restconf-all odl-ovsdb-library odl-restconf-all odl-ovsdb-southbound-api odl-ovsdb-southbound-impl odl-ovsdb-southbound-impl-rest
