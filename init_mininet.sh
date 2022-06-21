b05e5ccb26e9834905587ee06c2757a9a6348a42
rubenqc <rwquispec@gmail.com>
2022-06-21 12:27:03 -0500

    Setup initial configuration sdn-simulation

---
+++ init_mininet.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# start ovs service
+service openvswitch-switch start
+
+# start mininet scenario
+echo " * Custom: Start Mininet"
+mn --controller=remote,ip=172.17.0.2,port=6633  --switch ovs,protocols=OpenFlow13  --topo=single,4
+
+bash
